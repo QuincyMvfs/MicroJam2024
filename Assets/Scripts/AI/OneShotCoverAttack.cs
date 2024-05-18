6c6ff5b6b6a06bd7e806b1e542b105c639f729cf
navnxv <singh.navpreet14.1@gmail.com>
2024-05-18 19:00:48 +0530

    added timer for one shot

--- Assets/Scripts/AI/OneShotCoverAttack.cs
+++ Assets/Scripts/AI/OneShotCoverAttack.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class OneShotCoverAttack : MonoBehaviour
@@ -5,9 +6,18 @@ public class OneShotCoverAttack : MonoBehaviour
     [SerializeField] private float _damageAmount = 999999999.0f;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private GameObject _vfxPrefab;
+    [SerializeField] private float _warningTimer = 10.0f; 
 
     public void ExecuteOneShot()
     {
+        StartCoroutine(PerformOneShot());
+    }
+
+    IEnumerator PerformOneShot()
+    {
+        // Wait for the warning timer duration before executing the attack
+        yield return new WaitForSeconds(_warningTimer);
+
         Vector3 direction = (_playerTransform.position - transform.position).normalized;
         Ray ray = new Ray(transform.position, direction);
         RaycastHit hit;
@@ -20,7 +30,6 @@ public class OneShotCoverAttack : MonoBehaviour
                 health.Damage(_damageAmount, this.gameObject);
             }
 
-           
             // Draw the ray in the Scene view for debugging purposes
             Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red, 1f);
         }
@@ -29,6 +38,7 @@ public class OneShotCoverAttack : MonoBehaviour
             Debug.DrawRay(ray.origin, ray.direction * 5000000, Color.red, 1f);
         }
 
+        // Spawn the VFX at the current position
         if (_vfxPrefab != null)
         {
             Instantiate(_vfxPrefab, this.transform.position, Quaternion.identity);
