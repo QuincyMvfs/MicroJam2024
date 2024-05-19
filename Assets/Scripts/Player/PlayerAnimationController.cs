744afd31e2a0628a9fbec8a21fa61dfae5fbbe46
Quincy Michels <quincy.michels@gmail.com>
2024-05-18 20:03:02 -0400

    animator

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -2,7 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
+
 public class PlayerAnimationController : MonoBehaviour
 {
-    
+    private Animator _animator;
+    [SerializeField] private CharacterMovement _characterMovement;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _animator.SetFloat("Right", _characterMovement.CurrentDirection);
+    }
+
+
 }
