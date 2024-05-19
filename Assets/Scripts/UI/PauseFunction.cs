30ada1fe6d47d2d37b7ab1da4317429a7e89dd9c
Quincy Michels <quincy.michels@gmail.com>
2024-05-18 21:08:39 -0400

    pause menu works (escape)

--- Assets/Scripts/UI/PauseFunction.cs
+++ Assets/Scripts/UI/PauseFunction.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class PauseFunction : MonoBehaviour
 {
-    public GameObject PausePanel;
+    [SerializeField] private GameObject _pausePanel;
+    [SerializeField] private PlayerController _playerController;
+
+    public bool IsPaused => _isPaused;
+    private bool _isPaused = false;
 
     public void Pause()
     {
-        Debug.Log("paused");
-        PausePanel.SetActive(true);
-        Time.timeScale = 0;
+        _isPaused = !_isPaused;
+        if (_isPaused)
+        {
+            _playerController.StopMovement();
+            Debug.Log("paused");
+            _pausePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Continue();
+        }
     }
 
     public void Continue()
     {
+        _isPaused = false;
         Debug.Log("unpaused");
-        PausePanel.SetActive(false);
+        _pausePanel.SetActive(false);
         Time.timeScale = 1;
     }
 }
